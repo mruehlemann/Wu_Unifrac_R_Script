2098113fa96534bf4132fe0b7be16c1f86236e73
ruthgrace <ruthgracewong@gmail.com>
2016-03-20 16:56:01 -0400

    got breastmilk barplots working

--- breastmilk/breastmilk_script.r
+++ breastmilk/breastmilk_script.r
@@ -85,46 +85,42 @@ ratio_no_log.pc2.varEx <- sd(ratio_no_log.pcoa$vector[,2])*sd(ratio_no_log.pcoa$
 #save plots as PDF
 pdf("output/breastmilk_pcoa_plots.pdf")
 
+# MAKE BAR PLOTS
 
-# # MAKE BAR PLOTS
+#convert to dist structure
+unweighted.dist <- as.dist(unweighted)
+weighted.dist <- as.dist(weighted)
+information.dist <- as.dist(information)
 
-# #convert to dist structure
-# unweightedUnifrac.dist <- as.dist(unweightedUnifrac)
-# weightedUnifrac.dist <- as.dist(weightedUnifrac)
-# eUnifrac.dist <- as.dist(eUnifrac)
+#"average" is most similar to UPGMA, apparently
+unweighted.dendo <- hclust(unweighted.dist, method="average")
+weighted.dendo <- hclust(weighted.dist, method="average")
+information.dendo <- hclust(information.dist, method="average")
 
-# #"average" is most similar to UPGMA, apparently
-# unweightedUnifrac.dendo <- hclust(unweightedUnifrac.dist, method="average")
-# weightedUnifrac.dendo <- hclust(weightedUnifrac.dist, method="average")
-# eUnifrac.dendo <- hclust(eUnifrac.dist, method="average")
+#get otu proportions for barplot
+prop <- t(apply(breastmilk.otu.tab,1,function(x) x/sum(x)))
 
-# #get otu proportions for barplot
-# brazil.prop <- t(apply(breastmilk.otu.tab,1,function(x) x/sum(x)))
+# plot dendogram with bar plots
 
-#plot dendogram with bar plots
+#GG legacy code. Fix size, margins, position
+par(mfrow=c(2,1), mar=c(1, 3, 2, 1) + 0.1,cex=0.3)
 
-# #GG legacy code. Fix size, margins, position
-# par(mfrow=c(2,1), mar=c(1, 3, 2, 1) + 0.1,cex=0.3)
+plot(unweighted.dendo, axes=F, ylab=NULL, ann=F,hang=-1)
 
-# plot(unweightedUnifrac.dendo, axes=F, ylab=NULL, ann=F,hang=-1)
-
-# #order the barplot 
-# colors <- c("steelblue3","skyblue1", "indianred1", "mediumpurple1", "olivedrab3", "pink", "#FFED6F", "mediumorchid3", "ivory2", "tan1", "aquamarine3", "#C0C0C0", "royalblue4", "mediumvioletred", "#999933", "#666699", "#CC9933", "#006666", "#3399FF", "#993300", "#CCCC99", "#666666", "#FFCC66", "#6699CC", "#663366", "#9999CC", "#CCCCCC", "#669999", "#CCCC66", "#CC6600", "bisque", "#9999FF", "#0066CC", "#99CCCC", "#999999", "#FFCC00", "#009999", "#FF9900", "#999966", "#66CCCC", "#339966", "#CCCC33", "#EDEDED")
-# barplot(t(brazil.prop[unweightedUnifrac.dendo$order,]), space=0,col=colors, las=2)
-
-# plot(weightedUnifrac.dendo, axes=F, ylab=NULL, ann=F)
-# #order the barplot 
-# barplot(t(brazil.prop[weightedUnifrac.dendo$order,]), space=0,col=colors, las=2)
-
-# plot(eUnifrac.dendo, axes=F, ylab=NULL, ann=F)
-# #order the barplot 
-# barplot(t(brazil.prop[eUnifrac.dendo$order,]), space=0,col=colors, las=2)
-
-#plot(clrDirichletUniFrac.dendo, axes=F, ylab=NULL, ann=F)
 #order the barplot 
-#barplot(t(brazil.prop[clrDirichletUniFrac.dendo$order,]), space=0,col=colors, las=2)
+colors <- c("steelblue3","skyblue1", "indianred1", "mediumpurple1", "olivedrab3", "pink", "#FFED6F", "mediumorchid3", "ivory2", "tan1", "aquamarine3", "#C0C0C0", "royalblue4", "mediumvioletred", "#999933", "#666699", "#CC9933", "#006666", "#3399FF", "#993300", "#CCCC99", "#666666", "#FFCC66", "#6699CC", "#663366", "#9999CC", "#CCCCCC", "#669999", "#CCCC66", "#CC6600", "bisque", "#9999FF", "#0066CC", "#99CCCC", "#999999", "#FFCC00", "#009999", "#FF9900", "#999966", "#66CCCC", "#339966", "#CCCC33", "#EDEDED")
+barplot(t(prop[unweighted.dendo$order,]), space=0,col=colors, las=2)
 
-#par(plotParameters)
+plot(weighted.dendo, axes=F, ylab=NULL, ann=F)
+#order the barplot 
+barplot(t(prop[weighted.dendo$order,]), space=0,col=colors, las=2)
+
+plot(information.dendo, axes=F, ylab=NULL, ann=F)
+#order the barplot 
+barplot(t(prop[information.dendo$order,]), space=0,col=colors, las=2)
+
+
+par(plotParameters)
 
 
 
