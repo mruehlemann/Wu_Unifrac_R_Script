504cab9d30eb52c8b129df885ce20376ef8a75f8
ruthgrace <ruthgracewong@gmail.com>
2016-03-20 16:56:48 -0400

    got dendrogram labels to line up at the bottom

--- breastmilk/breastmilk_script.r
+++ breastmilk/breastmilk_script.r
@@ -105,17 +105,17 @@ prop <- t(apply(breastmilk.otu.tab,1,function(x) x/sum(x)))
 #GG legacy code. Fix size, margins, position
 par(mfrow=c(2,1), mar=c(1, 3, 2, 1) + 0.1,cex=0.3)
 
-plot(unweighted.dendo, axes=F, ylab=NULL, ann=F,hang=-1)
+plot(unweighted.dendo, axes=F, ylab=NULL, ann=F, hang=-1)
 
 #order the barplot 
 colors <- c("steelblue3","skyblue1", "indianred1", "mediumpurple1", "olivedrab3", "pink", "#FFED6F", "mediumorchid3", "ivory2", "tan1", "aquamarine3", "#C0C0C0", "royalblue4", "mediumvioletred", "#999933", "#666699", "#CC9933", "#006666", "#3399FF", "#993300", "#CCCC99", "#666666", "#FFCC66", "#6699CC", "#663366", "#9999CC", "#CCCCCC", "#669999", "#CCCC66", "#CC6600", "bisque", "#9999FF", "#0066CC", "#99CCCC", "#999999", "#FFCC00", "#009999", "#FF9900", "#999966", "#66CCCC", "#339966", "#CCCC33", "#EDEDED")
 barplot(t(prop[unweighted.dendo$order,]), space=0,col=colors, las=2)
 
-plot(weighted.dendo, axes=F, ylab=NULL, ann=F)
+plot(weighted.dendo, axes=F, ylab=NULL, ann=F, hang=-1)
 #order the barplot 
 barplot(t(prop[weighted.dendo$order,]), space=0,col=colors, las=2)
 
-plot(information.dendo, axes=F, ylab=NULL, ann=F)
+plot(information.dendo, axes=F, ylab=NULL, ann=F, hang=-1)
 #order the barplot 
 barplot(t(prop[information.dendo$order,]), space=0,col=colors, las=2)
 
