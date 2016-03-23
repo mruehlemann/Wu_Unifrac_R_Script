7de248f13768ab2150e5955534390405816c8c04
ruthgrace <ruthgracewong@gmail.com>
2016-03-23 11:04:13 -0400

    put plotting into function

--- tongue_sample_migration/plot_differences.R
+++ tongue_sample_migration/plot_differences.R
@@ -62,78 +62,82 @@ getVarExplained <- function(vector) {
 	return(varEx)
 }
 
+plotMigration <- function(d,e) {
+	d.varEx <- getVarExplained(d$vectors)
 
-d.varEx <- getVarExplained(d$vectors)
+	# Setup axis labelling conventions
+	per <- "% Explained"
+	x.pc1.explained <- d.varEx[1]
+	x.pc1.explained <- round(x.pc1.explained, digits=1)
+	xlabel <- "Principal Component1 Eigenvalues: "
+	xla <- paste(xlabel, x.pc1.explained,per, sep="", collapse=NULL)
 
-# Setup axis labelling conventions
-per <- "% Explained"
-x.pc1.explained <- d.varEx[1]
-x.pc1.explained <- round(x.pc1.explained, digits=1)
-xlabel <- "Principal Component1 Eigenvalues: "
-xla <- paste(xlabel, x.pc1.explained,per, sep="", collapse=NULL)
+	x.pc2.explained <- d.varEx[2]
+	x.pc2.explained <- round(x.pc2.explained, digits=1)
+	ylabel <- "Principal Component2 Eigenvalues: "
+	yla <- paste(ylabel, x.pc2.explained,per, sep="", collapse=NULL)
 
-x.pc2.explained <- d.varEx[2]
-x.pc2.explained <- round(x.pc2.explained, digits=1)
-ylabel <- "Principal Component2 Eigenvalues: "
-yla <- paste(ylabel, x.pc2.explained,per, sep="", collapse=NULL)
+	#perform procrustes fit
+	fit <- procrustes(d$vectors, e$vectors)
 
-#perform procrustes fit
-fit <- procrustes(d$vectors, e$vectors)
+	rtl <- NULL
+	ltr <- NULL
 
-rtl <- NULL
-ltr <- NULL
+	component <- 1
 
-component <- 1
-
-for (i in 1:60)	# Iterate through all 60 samples
-{
-	if ( (d$vectors[i,component] > 0) & (fit$Yrot[i,component] < 0))
-	{	
-		print("Right Cluster to Left Cluster Movement: ")	# Moved from right to left
-		print(rownames(d$vectors)[i])
-		rtl <- c(rtl, rownames(d$vectors)[i])	# Get list of Right to Left Movement
-	}	
-}
-for (i in 1:60)
-{
-	if ( (d$vectors[i,component] < 0) & (fit$Yrot[i,component] > 0))
+	for (i in 1:60)	# Iterate through all 60 samples
 	{
-		print("Left Cluster to Right Cluster Movement: ")	# Moved from left to right
-		print(rownames(d$vectors)[i])
-		ltr <- c(ltr, rownames(d$vectors)[i])	# Get list of Left to Right movement
+		if ( (d$vectors[i,component] > 0) & (fit$Yrot[i,component] < 0))
+		{	
+			print("Right Cluster to Left Cluster Movement: ")	# Moved from right to left
+			print(rownames(d$vectors)[i])
+			rtl <- c(rtl, rownames(d$vectors)[i])	# Get list of Right to Left Movement
+		}	
 	}
+	for (i in 1:60)
+	{
+		if ( (d$vectors[i,component] < 0) & (fit$Yrot[i,component] > 0))
+		{
+			print("Left Cluster to Right Cluster Movement: ")	# Moved from left to right
+			print(rownames(d$vectors)[i])
+			ltr <- c(ltr, rownames(d$vectors)[i])	# Get list of Left to Right movement
+		}
+	}
+
+	# First/second refer to the columns the data is being read from 
+	first <- 1
+	second <- 2
+
+	plot(d$vectors[1:60,first], d$vectors[1:60,second],main="Sample Migration between Rarefactions",xlab=xla,ylab=yla, pch=19, col=rgb(0,0,0,0.4))	# Plot the 1st rarefation
+
+	shapes <- c(19,15,19,15,19)
+	colours <- c(rgb(1,0,0,0.4), rgb(1,0,0,0.4), rgb(0,0,1,0.4), rgb(0,0,1,0.4), rgb(0,0,0,0.4) )
+
+	#legend(x=-0.25,y=0.31,title="Sample Movement", legend = c("Left->Right","Origin","Right->Left","Origin", "No Change"), pch=shapes, col=colours)
+
+	# Points that have moved from right to left are red
+	# Points that have moved from left to right are blue
+
+	# Points represent a plot of Rarefaction 1, but are coloured based on sample movement of Rare 2
+	points(d$vectors[rtl,first][fit$Yrot[rtl,first] < 0], d$vectors[rtl,second][fit$Yrot[rtl,first] < 0], pch=19, col=rgb(1,0,0,0.4))	# Red
+	points(d$vectors[ltr,first][fit$Yrot[ltr,first] > 0], d$vectors[ltr,second][fit$Yrot[ltr,first] > 0], pch=19, col=rgb(0,0,1,0.4))	# Blue
+
+	# The squares indicate WHERE they have moved from 
+	# Can comment this out to remove square drawing
+	points(fit$Yrot[rtl,first], fit$Yrot[rtl,second], pch = 15, col=rgb(1,0,0,0.4))
+	points(fit$Yrot[ltr,first], fit$Yrot[ltr,second], pch = 15, col=rgb(0,0,1,0.4))
+
+	# Draw line segments between the movements
+	arrows(fit$Yrot[rtl,first], fit$Yrot[rtl, second], d$vectors[rtl, first], d$vectors[rtl, second], col = rgb(1,0,0,0.5), length=0.1)
+
+	arrows(fit$Yrot[ltr,first], fit$Yrot[ltr, second], d$vectors[ltr, first], d$vectors[ltr, second], col = rgb(0,0,1,0.5), length=0.1)
 }
 
-# First/second refer to the columns the data is being read from 
-first <- 1
-second <- 2
+
 
 pdf("UniFrac_tvst_movement.pdf")	# Comment out if not plotting
-plot(d$vectors[1:60,first], d$vectors[1:60,second],main="Sample Migration between Rarefactions",xlab=xla,ylab=yla, pch=19, col=rgb(0,0,0,0.4))	# Plot the 1st rarefation
 
-shapes <- c(19,15,19,15,19)
-colours <- c(rgb(1,0,0,0.4), rgb(1,0,0,0.4), rgb(0,0,1,0.4), rgb(0,0,1,0.4), rgb(0,0,0,0.4) )
-
-#legend(x=-0.25,y=0.31,title="Sample Movement", legend = c("Left->Right","Origin","Right->Left","Origin", "No Change"), pch=shapes, col=colours)
-
-# Points that have moved from right to left are red
-# Points that have moved from left to right are blue
-
-# Points represent a plot of Rarefaction 1, but are coloured based on sample movement of Rare 2
-points(d$vectors[rtl,first][fit$Yrot[rtl,first] < 0], d$vectors[rtl,second][fit$Yrot[rtl,first] < 0], pch=19, col=rgb(1,0,0,0.4))	# Red
-points(d$vectors[ltr,first][fit$Yrot[ltr,first] > 0], d$vectors[ltr,second][fit$Yrot[ltr,first] > 0], pch=19, col=rgb(0,0,1,0.4))	# Blue
-
-# The squares indicate WHERE they have moved from 
-# Can comment this out to remove square drawing
-points(fit$Yrot[rtl,first], fit$Yrot[rtl,second], pch = 15, col=rgb(1,0,0,0.4))
-points(fit$Yrot[ltr,first], fit$Yrot[ltr,second], pch = 15, col=rgb(0,0,1,0.4))
-
-# Draw line segments between the movements
-arrows(fit$Yrot[rtl,first], fit$Yrot[rtl, second], d$vectors[rtl, first], d$vectors[rtl, second], col = rgb(1,0,0,0.5), length=0.1)
-
-arrows(fit$Yrot[ltr,first], fit$Yrot[ltr, second], d$vectors[ltr, first], d$vectors[ltr, second], col = rgb(0,0,1,0.5), length=0.1)
-
-
+plotMigration(d,e)
 
 dev.off()
 
